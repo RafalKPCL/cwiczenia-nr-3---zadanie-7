087d83f19a8c48b2e21965dc6e520c8f265cd1bf
RafalKPCL <chx100524@student.chorzow.wsb.pl>
2023-06-16 02:46:23 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = GetNumber("Podaj liczbę n: ");
+
+        Console.WriteLine($"Pary liczb naturalnych o sumie {n}:");
+        PrintNumberPairs(n);
+
+        Console.ReadKey();
+    }
+
+    static void PrintNumberPairs(int n)
+    {
+        for (int i = 1; i <= n / 2; i++)
+        {
+            int j = n - i;
+            Console.WriteLine($"{i}, {j}");
+        }
+    }
+
+    static int GetNumber(string message)
+    {
+        int number;
+        while (true)
+        {
+            Console.Write(message);
+            if (int.TryParse(Console.ReadLine(), out number) && number >= 0)
+            {
+                break;
+            }
+            Console.WriteLine("Błędne dane. Podaj liczbę naturalną.");
+        }
+
+        return number;
+    }
+}
+
